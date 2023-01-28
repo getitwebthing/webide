c41436e11bcd3d5a6ce4d37bd4daf1f0400ab299
getitwebthing <79367587+getitwebthing@users.noreply.github.com>
2023-01-28 15:51:28 +0000

    main

---
+++ test.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+public class test {
+    public static void main(String[] args) {
+        HashMap map = new HashMap();
+        map.put(123, "asd");
+
+        map.put("bn", 123);
+
+        System.out.println("halskjdhflhjasdf");
+
+    }
+
+
+}
